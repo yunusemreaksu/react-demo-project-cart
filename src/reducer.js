c279ed6de068b25b1f8bc40df64aaaae1a9121f2
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-21 16:40:57 +0300

    Added get totals and fetching data functions

--- src/reducer.js
+++ src/reducer.js
@@ -25,6 +25,29 @@ const reducer = (state, action) => {
         return {...state, cart: tempCart}
     }
 
+    if (action.type === "GET_TOTALS") {
+        let {total, amount} = state.cart.reduce((cartTotal, cartItem) => {
+            const {price, amount} = cartItem;
+            const itemTotal = price * amount
+            cartTotal.total += itemTotal
+            cartTotal.amount += amount;
+            return cartTotal
+        }, {
+            total: 0,
+            amount: 0,
+        })
+        total = parseFloat(total.toFixed(2))
+        return {...state, total, amount}
+    }
+
+    if (action.type === "LOADING") {
+        return {...state, loading: true}
+    }
+
+    if (action.type === "DISPLAY_ITEMS") {
+        return {...state, cart: action.payload, loading: false}
+    }
+
     return state
 }
 
