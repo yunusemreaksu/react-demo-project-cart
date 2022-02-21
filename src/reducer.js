1ea319058ef3ccfbbda03ec7e4f7d3e0b9b1c8a7
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-21 16:55:11 +0300

    Added toggle amount functions

--- src/reducer.js
+++ src/reducer.js
@@ -48,7 +48,22 @@ const reducer = (state, action) => {
         return {...state, cart: action.payload, loading: false}
     }
 
-    return state
+    if(action.type === "TOGGLE_AMOUNT") {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload.id) {
+                if (action.payload.type === "inc") {
+                    return {...cartItem, amount: cartItem.amount + 1}
+                }
+                if (action.payload.type === "dec") {
+                    return {...cartItem, amount: cartItem.amount - 1}
+                }
+            }
+            return cartItem
+        }).filter((cartItem) => cartItem.amount !== 0)
+        return {...state, cart: tempCart}
+    }
+
+    throw new Error("no matching action type")
 }
 
 export default reducer
